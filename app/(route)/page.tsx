7135357d3a3e7a9555b0a0cc37236b73d0dcbbd1
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-19 12:36:08 +0300

    ui in progress

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 // import { heading } from "@/tailwind-utils";
 
 
 export default function Home() {
   return (
     <>
-    <h1 className="heading">Home</h1>
+    <section className="pink_container">
+      <h1 className="heading">Pitch Your Startup, <br /> Connect with Enterpreneurs</h1>
+
+      <p className="sub-heading !max-w-3xl"> Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+
+      <SearchForm/>
+    </section>
+    
     </>
   );
 }
