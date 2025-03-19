eb217b74fced4a8a0bea33681392017e1ee88b99
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-19 12:05:40 +0300

    all reconfigured to next v-15.0.0

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { heading } from "@/tailwind-utils";
+// import { heading } from "@/tailwind-utils";
 
 
 export default function Home() {
   return (
     <>
-    <h1 className={`${heading} `}>Home</h1>
+    <h1 className="heading">Home</h1>
     </>
   );
 }
