f33d18d0ff355e5e8477eef5ea681b0c23eeb304
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-20 17:02:55 +0300

    fetching individual data

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -17,3 +17,21 @@ export const STARTUP_QUERY = defineQuery(`*[_type== "startup" && defined(slug.cu
     image
   
 }`);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`
+    *[_type == "startup" && _id == $id][0] {
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {
+        _id,
+        name,
+        image,
+        bio},
+    views,
+    description,
+    category,
+    image,
+    pitch
+    }`)
