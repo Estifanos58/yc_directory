56c54cca87de3d2b287802617905ada7ea23db3c
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-20 09:47:53 +0300

    sanity config and schema complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import 'easymde/dist/easymde.min.css'
 
 // Correct way to define localFont
 const workSans = localFont({
