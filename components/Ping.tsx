32fcea738c2b3ab43497be40d630abcaaf4cab0a
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-21 20:06:20 +0300

    real time view update ntegrated

--- components/Ping.tsx
+++ components/Ping.tsx
@@ -1,7 +1,15 @@
 
 const Ping = () => {
   return (
-    <div>Ping</div>
+    <div className="relative">
+      <div className="absolute -left-4 top-1">
+        <div className="flex size-[11px]">
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75">
+            <span className="relative inline-flex size-[11px] rounded-full bg-primary"></span>
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- components/View.tsx
+++ components/View.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import Ping from './Ping'
+import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { client } from '@/sanity/lib/client'
+import { writeClient } from '@/sanity/lib/write-client'
+import { unstable_after as after } from 'next/server'
 
-const View = ({id}:{id:string}) => {
+const View = async({id}:{id:string}) => {
+
+  const {views: Totalviews} = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id})
+ 
+  after(async ()=>  await writeClient.patch(id).set({views: Totalviews + 1}).commit())
+ 
+
   return (
     <div className="view-container">
         <div className="absolute -top-2 -right">
             <Ping/>
         </div>
         <p className="view-text">
-            <span className="font-black">100 views</span>
+            <span className="font-black">{formatNumber(Totalviews)}</span>
         </p>
     </div>
   )
 }
 
 export default View
+
+
+function formatNumber(num: number) {
+  if(num == 1) return `${num} view`;
+  else return `${num} views`;
+}
