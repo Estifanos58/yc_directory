4f12b159342f74e25e84b25eb5981091c899ae03
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2025-03-22 19:03:36 +0300

    create startup ui in the making

--- auth.ts
+++ auth.ts
@@ -7,8 +7,8 @@ import { writeClient } from "./sanity/lib/write-client"
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    async signIn({user, account, profile}) {
-      const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id: profile?.id})
+    async signIn({user,profile}) {
+      const existingUser = await client.withConfig({useCdn: false}).fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id: profile?.id})
 
       if(!existingUser){
         await writeClient.create({
@@ -27,7 +27,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
     async jwt({token, account, profile}) {
       if(account && profile){
-        const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id: profile?.id})
+        const user = await client.withConfig({useCdn:false}).fetch(AUTHOR_BY_GITHUB_ID_QUERY,{id: profile?.id})
 
         token.id = user?._id;
       }
